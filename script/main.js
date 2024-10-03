690c496e3f365bccefe7eaa3e9bd4c41412182be
M-12-10 <buttwahaj110@gmail.com>
2024-10-04 04:23:09 +0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -283,7 +283,7 @@ const animationTimeline = () => {
       y: 50,
       zIndex: "-1"
     })
-     // Add the gift box animation before .nine animation
+    .staggerFrom(".box", 1, ideaTextTrans, 1.2)
     .to(".box", 1, { // Animate the gift box to appear before ".nine"
       opacity: 1, 
       scale: 1, // Assume the initial scale was set to something smaller (like scale: 0 in CSS)
