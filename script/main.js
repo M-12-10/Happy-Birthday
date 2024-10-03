c6ad1ec3864adb7108b8330c239fbb35e834ccae
M-12-10 <buttwahaj110@gmail.com>
2024-10-04 03:40:08 +0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -283,6 +283,15 @@ const animationTimeline = () => {
       y: 50,
       zIndex: "-1"
     })
+    .staggerFrom(".temp h3", 1, ideaTextTrans, 1.2)
+    .to(
+      ".box",
+      0.5,
+      {
+        rotation: 360
+      },
+      "+=1"
+    ) 
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
       ".last-smile",
